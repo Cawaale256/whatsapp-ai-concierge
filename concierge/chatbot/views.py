88da7cc34df66a0ec527772f08b28218a788a5c7
Abdi Awale <abdi.awale5@gmail.com>
2025-05-29 20:05:15 +0000

    Define a basic API endpoint in chatbot/views.py

--- concierge/chatbot/views.py
+++ concierge/chatbot/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+
+def chatbot_response(request):
+    return JsonResponse({"message": "Hello! How can I assist you?"})
