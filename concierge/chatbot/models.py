d864a57b8adcbb532deb35a48cb543c04bf5d12e
Abdi Awale <abdi.awale5@gmail.com>
2025-06-25 10:50:39 +0000

    add travel_style and travel_buddy fields to TravelerProfile model

--- concierge/chatbot/models.py
+++ concierge/chatbot/models.py
@@ -11,6 +11,8 @@ class TravelerProfile(models.Model):
     phone_number = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=100, blank=True)
     last_destination = models.CharField(max_length=100, blank=True)
+    travel_style = models.CharField(max_length=100, blank=True)
+    travel_buddy = models.CharField(max_length=100, blank=True)
     preferences = models.TextField(blank=True)
 
     def __str__(self):
