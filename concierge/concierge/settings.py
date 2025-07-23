ab2ea4867ea8ca4f020482e892d139948b193a9b
Abdi Awale <abdi.awale5@gmail.com>
2025-07-23 12:30:18 +0000

    Add STATIC_ROOT to settings.py to fix collectstatic error

--- concierge/concierge/settings.py
+++ concierge/concierge/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'chatbot',  # Registering chatbot app
 ]
 
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -138,7 +139,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'chatbot/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
