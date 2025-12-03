0b9f4411c9993ec2591cd43c5688840daa5c6892
Abdi Awale <abdi.awale5@gmail.com>
2025-12-03 11:35:35 +0000

    Configure Whitenoise and static file paths

--- concierge/settings.py
+++ concierge/settings.py
@@ -150,6 +150,7 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'chatbot/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
